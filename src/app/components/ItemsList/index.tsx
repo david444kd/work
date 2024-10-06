2b42e04517476315c174a6414747acd3c34410c6
Давид Кожахметов <davidkozahmetov@MacBook-Air-David.local>
2024-10-06 23:36:20 +0500

    six

--- src/app/components/ItemsList/index.tsx
+++ src/app/components/ItemsList/index.tsx
@@ -48,7 +48,8 @@ export const ItemsList = ({ data }: { data: any[] }) => {
                 <CardHeader className="flex flex-col gap-3 ">
                   <div className="flex w-full items-center justify-between mb-1">
                     <div className="flex gap-3 items-center">
-                      <Zombie></Zombie>
+                      {/* <Zombie></Zombie> */}
+                      <Image className="w-10 h-10" src={item.imgTitle}></Image>
                       <div>
                         <p className="text-md">Brooklyn simons</p>
                         <p className="text-xs text-default-400">
